6bc2142744c53a7eeb3880e134414d9d594855dd
Peter Haahr Bøgh Larsen <20083878@post.au.dk>
2024-03-18 12:23:20 +0100

    Display and Door classes filled out

--- src/Ladeskab.lib/Interfaces/IDoor.cs
+++ src/Ladeskab.lib/Interfaces/IDoor.cs
@@ -1,8 +1,12 @@
-﻿namespace Ladeskab.lib.Interfaces
+﻿using static Ladeskab.lib.Interfaces.Door;
+
+namespace Ladeskab.lib.Interfaces
 {
     public interface IDoor
     {
         void LockDoor();
         void UnlockDoor();
+
+        event EventHandler<DoorEventArgs>? DoorEvent;
     }
 }
